7f6ddadefa5f46d8c5b6c12b4559dba9ca9bb60d
Dhruv Menon <dhruvmenon1104@gmail.com>
2025-05-06 14:58:55 +0530

    fix: add missing package bs4

--- optics_framework/helper/setup.py
+++ optics_framework/helper/setup.py
@@ -23,7 +23,7 @@ ACTION_DRIVERS = DriverCategory(
     drivers={
         "Appium": DriverPackage(name="Appium", packages=["appium-python-client"]),
         "BLE": DriverPackage(name="BLE", packages=["pyserial"]),
-        "Selenium": DriverPackage(name="Selenium", packages=["selenium"])
+        "Selenium": DriverPackage(name="Selenium", packages=["selenium", "bs4"])
     }
 )
 
