46cfa4527797eb92ab1a3d0fff6108eb68c6e236
Dhruv Menon <dhruvmenon1104@gmail.com>
2025-10-16 12:14:57 +0530

    fix: update app package handling to use bundleId in Appium driver
    Signed-off-by: Dhruv Menon <dhruvmenon1104@gmail.com>

--- optics_framework/engines/drivers/appium.py
+++ optics_framework/engines/drivers/appium.py
@@ -101,7 +101,8 @@ class Appium(DriverInterface):
         if app_package:
             all_caps["appPackage"] = app_package
             all_caps["appium:appPackage"] = app_package
-            all_caps["app"] = app_package
+            all_caps["appium:bundleId"] = app_package
+            all_caps["bundleId"] = app_package
         if app_activity:
             all_caps["appActivity"] = app_activity
             all_caps["appium:appActivity"] = app_activity
